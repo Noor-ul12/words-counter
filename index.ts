e04cca4640711d944b459e3a583f5100dc3f3396
Noor ul <hudanoor714,com>
2024-05-12 15:26:31 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+//import the inquirer module which is command line interface for node.js
+import inquirer from "inquirer";
+
+// Declare a costant" answer" and assingn it to the result of the 'inquirer.prompt' funtion
+
+const answer: {
+
+    sentence: string
+} =await inquirer.prompt([
+    { name: "sentence",
+      type: "input",
+      message: "write your sentence to count the words"
+
+    }
+])
+
+const words = answer.sentence.trim().split(" ");
+
+// print the array of words to the console
+
+console.log(words)
+
+// print the words count of the sentence
+
+console.log(`your sentence words count is ${words.length}`);
